1a7842857b3411c985e10e67e482aec84f5edb0b
dell-optiplex-790 <dell.optiplex.d4719126@gmail.com>
2024-10-09 16:31:36 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: './index.js', // Your entry point
   output: {
     filename: 'fbin.js', // Output file name
-    path: path.resolve(__dirname, 'tmp'), // Output directory
+    path: 'tmp', // Output directory
     libraryTarget: 'commonjs2', // Support Node.js module format
   },
   optimization: {
